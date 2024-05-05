afe8f7681e4411b2a4240095a4e24272ad0e3f84
Marcellino Abdelmalak <me@excalian.com>
2024-05-05 13:06:25 -0700

    Add website

---
+++ astro.config.mjs
@@ -0,0 +1,44 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://docs.excalian.com',
+	integrations: [
+    	starlight({
+			title: "Excalian's Documentation",
+			description: "All documentation for all products or projects related to Excalian",
+			logo: { src: './public/logo.png' },
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						{label: 'Example', link: '/guides/example'},
+					],
+				},
+				{
+					label: 'Reference',
+					items: [
+						{label: 'Example', link: '/reference/example'},
+					],
+				},
+			],
+			social: {
+				github: 'https://www.excalian.com/github',
+				youtube: 'https://www.excalian.com/youtube',
+				twitch: 'https://www.excalian.com/twitch',
+				'x.com': 'https://www.excalian.com/x',
+				threads: 'https://www.excalian.com/threads',
+				discord: 'https://www.excalian.com/discord',
+			},
+      customCss: [ './src/tailwind.css' ],
+      lastUpdated: false,
+	  favicon: './favicon.ico',
+		}),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
+});
